702c7b828b8eb7c07705b010cb5aa0985e35ceec
Brijesh <brijesh@Brijeshs-MacBook-Air.local>
2025-02-02 01:27:23 +0530

    feat(debounce): implement full solution

--- src/Components/Debounce/Debounce.tsx
+++ src/Components/Debounce/Debounce.tsx
@@ -1,44 +1,34 @@
 import { useState } from 'react';
 
+let timer: ReturnType<typeof setTimeout> | undefined;   // very important outside of function
 export default function Debounce() {
   const [search, setSearch] = useState('');
   const [list, setList] = useState<Array<string>>([]);
 
   const fetchList = async (id: string) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/comments/${id}`);
     const data = await response.json();
-    const nameList = data.map((item: any) => item.username);
+    console.log('fecth call for',id);
+    const nameList = [data?.body];
     setList(nameList);
   }
 
-  // const debounce = (func: Function, delay: number) => {
-  //   let timer: any;
-  //   return function (...args: any[]) {
-  //     const context = this;
-  //     if (timer) clearTimeout(timer);
-  //     timer = setTimeout(() => {
-  //       timer = null;
-  //       func.apply(context, args);
-  //     }, delay);
-  //   }
-  // }
-
   const debounce = (callback: any, delay: number) => {
-    return () => {
-      setTimeout(() => {
-        callback()
-      },delay)
+    return (...args: any[]) => {
+      if(timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback(...args);
+      },delay);
     }
   }
 
-
   const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const { value } = e?.target as HTMLInputElement;
-    if (value?.length > 0) {
-      setSearch(value);
-      debounce(fetchList, 500)(value);
-    } else {
-      setSearch('');
+    setSearch(value);
+  
+    if (value.length > 0) {
+      const searchItem = debounce(fetchList, 500);
+      searchItem(value);
     }
   }
 
@@ -53,14 +43,17 @@ export default function Debounce() {
           onKeyUp={e => handleSearch(e)}
         />
         {
-          search?.length > 0 && list.length > 0 &&
-          <div className="divide-y rounded-lg shadow w-full">
-            <ul className="py-2 text-sm text-gray-700">
-              <li className="block px-4 py-2 hover:bg-gray-300">
-                <span>Dashboard</span>
-              </li>
-            </ul>
-          </div>
+          search?.length > 0 && list.length > 0 && (
+            <div className="mt-2 divide-y rounded-lg shadow w-full bg-white">
+              <ul className="py-2 text-sm text-gray-700">
+                {list?.map((item: string, index: number) => (
+                  <li className="block px-4 py-2 hover:bg-gray-100 cursor-pointer transition-colors" key={index}>
+                    <span className="line-clamp-2">{item}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )
         }
       </div>
     </div>
